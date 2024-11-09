d4f7a65049802b82cf426ec9111a5bdf7cbc1275
BakondiKarina <karina.bakondi26@gmail.com>
2024-11-09 14:14:38 +0100

    Add files via upload

---
+++ hazi-karina-11-09/script.js
@@ -0,0 +1,20 @@
+console.log("Karina")
+
+let city = "Felsőtárkány"
+let country = "Magyarország"
+console.log(city + country)
+
+let x = 9;
+console.log(x)
+console.log(x + 1)
+
+let y = 20;
+console.log(x + y)
+console.log(x * y)
+
+let z = 2641;
+console.log(z % 7)
+
+let favouriteAnimal = prompt("Mi a kedvenc állatod?")
+console.log("A kedvenc állatod a " + favouriteAnimal)
+
